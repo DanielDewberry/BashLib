43b54b998b8851131cc3461ac61c873f0e4cdfd1
Daniel W Dewberry <Daniel.W.Dewberry@gmail.com>
2021-04-25 23:22:07 +0100

    make-executable.sh: add examples/

--- make-executable.sh
+++ make-executable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-find src tests -type f -not -name '*swp' -exec chmod 754 {} \;
+find                  \
+    src               \
+    tests             \
+    examples          \
+    -type f           \
+    -not -name '*swp' \
+    -exec chmod 754 {} \;
 
