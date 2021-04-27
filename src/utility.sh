c94c08e1273b3b02925a478775c69ea5058f1a1a
Daniel W Dewberry <Daniel.W.Dewberry@gmail.com>
2021-04-27 21:16:31 +0100

    add utility: requires-root

---
+++ src/utility.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# -*- coding: utf-8 -*-
+
+
+requires-root()
+{
+    # Check user has root privileges
+    # 
+    # Returns 0 on success, exit 99 otherwise
+
+    if (( EUID != 0 )); then
+        echo 'Must run as root' >&2
+        exit 99
+    fi
+    return 0
+}
+
